36df7176cc9260755139f94b2e94570beeeae199
panica <max.panica@gmail.com>
2014-06-15 00:47:27 +0300

    fixator

--- src/helloworld/Helloworld.java
+++ src/helloworld/Helloworld.java
@@ -16,7 +16,7 @@ public class Helloworld {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.err.println("HELLOWORLD");
     }
     
 }
